ecebe6f35c635aa7257dbdebe8dadf65904aa71e
Vishaljha12 <91270374+Vishaljha12@users.noreply.github.com>
2025-10-05 18:58:51 +0530

    THIS my game

---
+++ m.py
@@ -0,0 +1,26 @@
+b=str(input("PLEASE ENTER 'Y' TO START WORD GUESSER GAME:"))
+
+  
+if(b=="Y"):
+  i=1
+  a=int(input("PLEASE ENTER A NO:"))
+  
+    
+    
+  if(a==1):
+    print("YOU WON!")
+  else:
+  
+    print("YOU LOST!")
+      
+  
+  
+
+
+
+else:
+    print("INVALID!")
+    
+    
+    
+
